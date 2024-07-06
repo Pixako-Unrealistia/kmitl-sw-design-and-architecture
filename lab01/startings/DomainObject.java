2aa48b5750b1564306949ce90032a1c254f3ef44
Pixako <62527607+Pixako-Unrealistia@users.noreply.github.com>
2024-07-06 18:59:35 +0700

    initial commit

---
+++ lab01/startings/DomainObject.java
@@ -0,0 +1,18 @@
+public class DomainObject {
+
+	public DomainObject (String name)	{
+		_name = name;
+	};
+
+	public DomainObject ()	{};
+
+	public String name ()	{
+		return _name;
+	};
+
+	public String toString() {
+		return _name;
+	};
+
+	protected String _name = "no name";
+}

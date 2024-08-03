f30f317baa96b632660dad73a4e545ecc9f1c724
ZenThaiDev <49715907+ZenThaiDev@users.noreply.github.com>
2024-08-04 00:27:16 +0700

    fix: runtime error

--- lab05/task2/Slot.java
+++ lab05/task2/Slot.java
@@ -1,79 +1,86 @@
 import java.util.ArrayList;
 
 public abstract class Slot {
-	
-	Cabinet cabinet;
-	Display display;
-	String name;
-	GPU gpu;
-	Payment payment;
-	OS os;
-	String software = "default software";
-	ArrayList components = new ArrayList();
-	
 
-	SlotComponentFactory componentFactory;
-	final void build(String cabi, String pay, String disp, String gp, String o) {
-		System.out.println("building the slot machine");
+    Cabinet cabinet;
+    Display display;
+    String name;
+    GPU gpu;
+    Payment payment;
+    OS os;
+    String software = "default software";
+    ArrayList components = new ArrayList();
+
+
+    SlotComponentFactory componentFactory = new SlotComponentFactoryImpl();
+
+    final void build(String cabi, String pay, String disp, String gp, String o) {
+        System.out.println("building the slot machine");
         cabinet = componentFactory.createCabinet(cabi);
         display = componentFactory.createDisplay(disp);
-        os = componentFactory.createOS(gp);
+        os = componentFactory.createOS(o);
         payment = componentFactory.createPayment(pay);
-        gpu = componentFactory.createGPU(o);
-	}
-	
-	void collectParts() {
-		System.out.println("fetching components:");
-	}
-	void assembleParts() {
-		System.out.println("assembling components");
-	}
-	void test(String type) {
-		if (type.equals("hardware")) System.out.println("testing hardware");
-		else System.out.println("testing software");
-	}
-	void uploadSoftware() {
-		System.out.println("uploading software:" + software);
-	}
-	void ship() {
-		System.out.println("wrapping the slot machine in Ceram Wrap and shipping it with UPS");
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String n) {
-		name=n;
-	}
+        gpu = componentFactory.createGPU(gp);
+    }
 
-	
-	public String toString() {
-		StringBuffer result = new StringBuffer();
-		result.append("---- " + name + " ----\n");
-		if (cabinet != null) {
-			result.append(cabinet);
-			result.append("\n");
-		}
-		if (display != null) {
-			result.append(display);
-			result.append("\n");
-		}
+    void collectParts() {
+        System.out.println("fetching components:");
+    }
 
-		if (gpu != null) {
-			result.append(gpu);
-			result.append("\n");
-		}
+    void assembleParts() {
+        System.out.println("assembling components");
+    }
 
-		if (payment != null) {
-			result.append(payment);
-			result.append("\n");
-		}
+    void test(String type) {
+        if (type.equals("hardware")) System.out.println("testing hardware");
+        else System.out.println("testing software");
+    }
 
-		if (os != null) {
-			result.append(os);
-			result.append("\n");
-		}
+    void uploadSoftware() {
+        System.out.println("uploading software:" + software);
+    }
+
+    void ship() {
+        System.out.println("wrapping the slot machine in Ceram Wrap and shipping it with UPS");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
 
 
-		return result.toString();
-	}
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append("---- " + name + " ----\n");
+        if (cabinet != null) {
+            result.append(cabinet);
+            result.append("\n");
+        }
+        if (display != null) {
+            result.append(display);
+            result.append("\n");
+        }
+
+        if (gpu != null) {
+            result.append(gpu);
+            result.append("\n");
+        }
+
+        if (payment != null) {
+            result.append(payment);
+            result.append("\n");
+        }
+
+        if (os != null) {
+            result.append(os);
+            result.append("\n");
+        }
+
+
+        return result.toString();
+    }
 }
--- lab05/task2/SlotComponentFactoryImpl.java
+++ lab05/task2/SlotComponentFactoryImpl.java
@@ -2,77 +2,77 @@
 
 public class SlotComponentFactoryImpl implements SlotComponentFactory {
 
-	@Override
-	public Cabinet createCabinet(String cabi) {
-		if (cabi.equals("Medium")) {
-			return new mediumCabinet();
-		} else if (cabi.equals("Large")) {
-			return new largeCabinet();
-		} else if (cabi.equals("Small")) {
-			return new smallCabinet();
-		} else {
-			throw new UnsupportedOperationException(cabi + " is not supported");
-		} 
-	}
+    @Override
+    public Cabinet createCabinet(String cabi) {
+        if (cabi.equals("Medium")) {
+            return new mediumCabinet();
+        } else if (cabi.equals("Large")) {
+            return new largeCabinet();
+        } else if (cabi.equals("Small")) {
+            return new smallCabinet();
+        } else {
+            throw new UnsupportedOperationException(cabi + " is not supported");
+        }
+    }
 
-	@Override
-	public Payment createPayment(String pay) {
-		if (pay.equals("Coins")) {
-			return new Coins();
-		} else if (pay.equals("Bills")) {
-			return new Bills();
-		} else if (pay.equals("TicketInTicketout")) {
-			return new TicketInTicketOut();
-		} else {
-			throw new UnsupportedOperationException(pay + " is not supported");
-		}
-	}
+    @Override
+    public Payment createPayment(String pay) {
+        if (pay.equals("Coins")) {
+            return new Coins();
+        } else if (pay.equals("Bills")) {
+            return new Bills();
+        } else if (pay.equals("TicketInTicketOut")) {
+            return new TicketInTicketOut();
+        } else {
+            throw new UnsupportedOperationException(pay + " is not supported");
+        }
+    }
 
-	@Override
-	public Display createDisplay(String disp) {
-		if (disp.equals("CRT")) {
-			return new CRT();
-		} else if (disp.equals("LCD")) {
-			return new LCD();
-		} else if (disp.equals("reels")) {
-			return new Reels();
-		} else if (disp.equals("VGA")) {
-			return new VGA();
-		} else {
-			throw new UnsupportedOperationException(disp + " is not supported");
-		}
+    @Override
+    public Display createDisplay(String disp) {
+        if (disp.equals("CRT")) {
+            return new CRT();
+        } else if (disp.equals("LCD")) {
+            return new LCD();
+        } else if (disp.equals("Reels")) {
+            return new Reels();
+        } else if (disp.equals("VGA")) {
+            return new VGA();
+        } else {
+            throw new UnsupportedOperationException(disp + " is not supported");
+        }
 
-	}
+    }
 
-	@Override
-	public GPU createGPU(String gp) {
-		if (gp.equals("ARM")) {
-			return new ARM();
-		} else if (gp.equals("X86")) {
-			return new X86();
-		} else if (gp.equals("X77")) {
-			return new X77();
-		} else {
-			throw new UnsupportedOperationException(gp + " is not supported");
-		}
-	}
+    @Override
+    public GPU createGPU(String gp) {
+        if (gp.equals("ARM")) {
+            return new ARM();
+        } else if (gp.equals("X86")) {
+            return new X86();
+        } else if (gp.equals("X77")) {
+            return new X77();
+        } else {
+            throw new UnsupportedOperationException(gp + " is not supported");
+        }
+    }
 
-	@Override
-	public OS createOS(String o) {
-		if (o.equals("linux")) {
-			return new Linux();
-		} else if (o.equals("windowsME")) {
-			return new WindowsME();
-		} else if (o.equals("windowsXP")) {
-			return new WindowsXP();
-		} else if (o.equals("android")) {
-			return new Android();
-		} else if (o.equals("symbian")) {
-			return new Symbian();
-		} else {
-			throw new UnsupportedOperationException(o + " is not supported");
-		}
+    @Override
+    public OS createOS(String o) {
+        if (o.equals("Linux")) {
+            return new Linux();
+        } else if (o.equals("WindowsME")) {
+            return new WindowsME();
+        } else if (o.equals("WindowsXP")) {
+            return new WindowsXP();
+        } else if (o.equals("Android")) {
+            return new Android();
+        } else if (o.equals("Symbian")) {
+            return new Symbian();
+        } else {
+            throw new UnsupportedOperationException(o + " is not supported");
+        }
 
-	}
+    }
 
 }

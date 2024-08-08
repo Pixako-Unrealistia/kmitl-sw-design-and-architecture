00c4e6226c133714b44cc5a42f88024aba88982f
JRsssssss <tackelzaza@gmail.com>
2024-08-08 22:02:16 +0700

    adding TeaFacada for lab06 task3

--- lab06/task3/MakeTea.java
+++ lab06/task3/MakeTea.java
@@ -4,17 +4,15 @@ import lab06.task3.facade.Tea;
 import lab06.task3.facade.TeaCup;
 import lab06.task3.facade.TeaInfuser;
 import lab06.task3.facade.Water;
+import lab06.task3.TeaFacade;
 
 public class MakeTea {
     public static void main(String[] args) {
         TeaCup blueCup = new TeaCup();
         Water water = new Water();
         TeaInfuser infuser = new TeaInfuser();
-        Tea tea = new Tea("Eral Grey");
-        infuser.addTea(tea);
-        water.boilWater();
-        blueCup.addWater(water);
-        blueCup.setSteepTime(15);
-        blueCup.steep();
+        // Tea tea = new Tea("Eral Grey");
+        TeaFacade teaMaker = new TeaFacade(blueCup,water,infuser);
+        teaMaker.makeTea("Eral Grey");
     }
 }
---
+++ lab06/task3/TeaFacade.java
@@ -0,0 +1,26 @@
+package lab06.task3;
+import lab06.task3.facade.Tea;
+import lab06.task3.facade.TeaCup;
+import lab06.task3.facade.TeaInfuser;
+import lab06.task3.facade.Water;
+
+public class TeaFacade {
+    private TeaCup cup;
+    private Water water;
+    private TeaInfuser infuser;
+
+    public TeaFacade(TeaCup cup, Water water, TeaInfuser infuser) {
+        this.cup = cup;
+        this.water = water;
+        this.infuser = infuser;
+    }
+
+    public void makeTea(String teaType) {
+        Tea tea = new Tea(teaType);
+        infuser.addTea(tea);
+        water.boilWater();
+        cup.addWater(water);
+        cup.setSteepTime(15);
+        cup.steep();
+    }
+}

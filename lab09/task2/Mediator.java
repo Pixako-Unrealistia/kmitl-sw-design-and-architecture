9a49f266130d54429a908ea8da4bb6b56e3c6adb
m0owo <8breadsticks8@gmail.com>
2024-09-16 18:44:34 +0700

    fix: americanDollar $$$

--- lab09/task2/Mediator.java
+++ lab09/task2/Mediator.java
@@ -18,7 +18,7 @@ public class Mediator implements AbstractMediator {
     };
 
     public boolean placeBid(float bid, String unitOfCurrency) {
-        float america_dollar = this.dollarConverter.convertCurrencyToDollars(bid, unitOfCurrency);
-        return this.americanSeller.isBidAccepted(america_dollar);
+        float americanDollar = this.dollarConverter.convertCurrencyToDollars(bid, unitOfCurrency);
+        return this.americanSeller.isBidAccepted(americanDollar);
     };
 }

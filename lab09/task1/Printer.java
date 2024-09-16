21efa75059765b54d5a27bfe4d95a8fc8c6bf5a1
Tripong Khemasurijunyo <tripong123@hotmail.com>
2024-09-16 19:21:18 +0700

    fix: system.err.println

--- lab09/task1/Printer.java
+++ lab09/task1/Printer.java
@@ -7,11 +7,11 @@ public class Printer {
     }
 
     public synchronized void print_line(int x1, int y1, int x2, int y2) {
-        System.err.println("Print in printer Line: x1=" + x1 + " y1=" + y1 +" to"+" x2=" + x2 + " y2=" + y2);
+        System.out.println("Print in printer Line: x1=" + x1 + " y1=" + y1 +" to"+" x2=" + x2 + " y2=" + y2);
     }
 
     public synchronized void print_pixel(int x, int y) {
-        System.err.println("Print in printer Pixel: x=" + x + " y=" + y);
+        System.out.println("Print in printer Pixel: x=" + x + " y=" + y);
     }
 
     public synchronized void print_circle(int x, int y, int r) {
--- lab09/task1/Screen.java
+++ lab09/task1/Screen.java
@@ -7,11 +7,11 @@ public class Screen {
     }
 
     public synchronized void draw_line(int x1, int y1, int x2, int y2) {
-        System.err.println("Display in Screen Line: x1=" + x1 + " y1=" + y1 +" to"+ " x2=" + x2 + " y2=" + y2);
+        System.out.println("Display in Screen Line: x1=" + x1 + " y1=" + y1 +" to"+ " x2=" + x2 + " y2=" + y2);
     }
 
     public synchronized void draw_pixel(int x, int y) {
-        System.err.println("Display in Screen Pixel: x=" + x + " y=" + y);
+        System.out.println("Display in Screen Pixel: x=" + x + " y=" + y);
     }
 
     public synchronized void draw_circle(int x, int y, int r) {

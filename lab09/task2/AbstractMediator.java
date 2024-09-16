05b386c7aa56a3a21a0e4aa1a522b9c23bac8b69
Music Auyeung <8breadsticks8@gmail.com>
2024-09-16 18:40:26 +0700

    fix: move methods to concrete mediator and change abstract to interface

--- lab09/task2/AbstractMediator.java
+++ lab09/task2/AbstractMediator.java
@@ -1,21 +1,3 @@
-public abstract class AbstractMediator {
-    AmericanSeller americanSeller;
-    FrenchBuyer frenchBuyer;
-    SwedishBuyer swedishBuyer;
-    DollarConverter dollarConverter;
-
-    public void registerAmericanSeller(AmericanSeller americanSeller) {
-        this.americanSeller = americanSeller;
-    };
-    public void registerFrenchBuyer(FrenchBuyer frenchBuyer) {
-        this.frenchBuyer = frenchBuyer;
-    };
-    public void registerSwedishBuyer(SwedishBuyer swedishBuyer) {
-        this.swedishBuyer = swedishBuyer;
-    };
-    public void registerDollarConverter(DollarConverter dollarConverter) {
-        this.dollarConverter = dollarConverter;
-    };
-    
+public interface AbstractMediator {
     public abstract boolean placeBid(float bid, String unitOfCurrency);
 }
--- lab09/task2/Mediator.java
+++ lab09/task2/Mediator.java
@@ -1,4 +1,22 @@
-public class Mediator extends AbstractMediator {
+public class Mediator implements AbstractMediator {
+    private AmericanSeller americanSeller;
+    private FrenchBuyer frenchBuyer;
+    private SwedishBuyer swedishBuyer;
+    private DollarConverter dollarConverter;
+
+    public void registerAmericanSeller(AmericanSeller americanSeller) {
+        this.americanSeller = americanSeller;
+    };
+    public void registerFrenchBuyer(FrenchBuyer frenchBuyer) {
+        this.frenchBuyer = frenchBuyer;
+    };
+    public void registerSwedishBuyer(SwedishBuyer swedishBuyer) {
+        this.swedishBuyer = swedishBuyer;
+    };
+    public void registerDollarConverter(DollarConverter dollarConverter) {
+        this.dollarConverter = dollarConverter;
+    };
+
     public boolean placeBid(float bid, String unitOfCurrency) {
         float america_dollar = this.dollarConverter.convertCurrencyToDollars(bid, unitOfCurrency);
         return this.americanSeller.isBidAccepted(america_dollar);
